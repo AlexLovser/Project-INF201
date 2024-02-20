1341a26c70d90fcf2bfa2bba66c86442bd074813
Elkatra2 <yassine.elkortbi@gmail.com>
2024-02-20 19:46:27 +0100

    added testing for "tourner_case"

--- test.ml
+++ test.ml
@@ -1,16 +1,22 @@
 #use "rendu_etd.ml" ;;
 Sys.command "clear" ;;
 
-print_endline "Test tourner_case" ;;
+(* Func for testing  *)
+let case_identique ((c11,c12,c13):case) ((c21,c22,c23):case): bool =
+  c11 == c21 && c12 == c22 && c13 == c23 
+;;
 
-let tour_0 : case = (2, -1, -1) ;;
-let tour_1 = tourner_case 1 tour_0 ;;
+(* Testing "tourner_case" *)
+let test_case_0 : case = ( 2, -1, -1) ;;
+let test_case_1 : case = ( 1, -2,  1) ;;
+let test_case_2 : case = (-1, -1,  2) ;;
+let test_case_3 : case = (-2,  1,  1) ;;
+let test_case_4 : case = (-1,  2, -1) ;;
+let test_case_5 : case = ( 1,  1, -2) ;;
 
-let tour_2 = tourner_case 1 tour_1 ;;
-
-let tour_3 = tourner_case 1 tour_2 ;;
-
-let tour_4 = tourner_case 1 tour_3 ;;
-
-let tour_5 = tourner_case 1 tour_4 ;;
-let tour_6 = tourner_case 12 tour_0 ;;
+assert (case_identique (tourner_case 1 test_case_0) test_case_1) ;;
+assert (case_identique (tourner_case 2 test_case_0) test_case_2) ;;
+assert (case_identique (tourner_case 3 test_case_0) test_case_3) ;;
+assert (case_identique (tourner_case 4 test_case_0) test_case_4) ;;
+assert (case_identique (tourner_case 5 test_case_0) test_case_5) ;;
+assert (case_identique (tourner_case 6 test_case_0) test_case_0) ;;
