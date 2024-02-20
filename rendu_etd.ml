9ff6276118d59ff61611fc799c3e06c204b1bc88
Elkatra2 <yassine.elkortbi@gmail.com>
2024-02-20 20:25:54 +0100

    some change in "rendu_etd"

--- rendu_etd.ml
+++ rendu_etd.ml
@@ -32,6 +32,7 @@ let est_dans_losange ((i,j,k):case) (dim:dimension): bool =
   -dim <= j && j <= dim && -dim <= k && k <= dim 
 ;;          
 
+(* A MODIFIER en Q3 *)
 let est_dans_losange_2 ((i,j,k):case) (dim:dimension): bool = 
   -dim <= i && i <= dim && -dim <= k && k <= dim 
 ;;   
@@ -40,7 +41,6 @@ let est_dans_losange_3 ((i,j,k):case) (dim:dimension): bool =
   -dim <= i && i <= dim && -dim <= j && j <= dim 
 ;;   
 
-(* A MODIFIER en Q3 *)
 let est_dans_etoile (c:case) (dim:dimension): bool =
   est_dans_losange c dim ||
   est_dans_losange_2 c dim ||
@@ -48,7 +48,7 @@ let est_dans_etoile (c:case) (dim:dimension): bool =
 ;;
 
 (* QUESTION 4 *)
-let [@warning "-8"] rec tourner_case (m:int) ((i,j,k):case): case =
+let rec tourner_case (m:int) ((i,j,k):case): case =
   let mm = m mod 6 in
     match mm with
     | 0 -> i, j, k
@@ -57,18 +57,12 @@ let [@warning "-8"] rec tourner_case (m:int) ((i,j,k):case): case =
 
 (*Question 5*)
 let translate ((c1,c2,c3):case) ((v1,v2,v3):vecteur): case =
-  let i = c1 + v1
-  and j = c2 + v2
-  and k = c3 + v3 
-    in i, j, k 
+  c1 + v1, c2 + v2, c3 + v3 
 ;;
 
 (*Question 6*)
 let diff_case ((c11,c12,c13):case) ((c21,c22,c23):case): vecteur =
-  let v1 = c11 - c21
-  and v2 = c12 - c22
-  and v3 = c13 - c23 
-    in v1, v2, v3
+  c11 - c21, c12 - c22, c13 - c23 
 ;;
 
 (* Question 7 *)
@@ -90,7 +84,9 @@ let calcul_pivot (c1:case) (c2:case): case option =
 
 (* Question 9 *)
 let vec_et_dist (c1:case) (c2:case): vecteur * int =
-  (0,0,0), 0 (* TODO *)
+  let v = (0,0,0)
+  and d = 0
+    in v, d
 ;;
 
 (* AFFICHAGE (fonctionne si les fonctions au dessus sont remplies) *)
