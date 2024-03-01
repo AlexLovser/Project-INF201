b4c4e93c287238d42901fd5c874a5fe530403ed5
Elkatra2 <yassine.elkortbi@gmail.com>
2024-03-01 19:59:26 +0100

    ....

--- projet_04_03_2024/inf201_ElKortbi_Tabolskii_Bendouha_Caille_Crelerot_Q1-Q9.ml
+++ projet_04_03_2024/inf201_ElKortbi_Tabolskii_Bendouha_Caille_Crelerot_Q1-Q9.ml
@@ -1,9 +1,12 @@
-(* ---------------------------------------------------------------------
+(* -----------------------------------------------------------------------------
    inf201_El_Kortbi_Tabolskii_Q1-Q9.ml : cr Q1 Q9 projet: Groupe ima4_C
 
    Yassin El Kortbi    <elkortby@etu.univ-grenoble-alpes.fr> 
    Aleksandr Tabolskii <aleksandr.tabolskii@etu.univ-grenoble-alpes.fr>  
-  ---------------------------------------------------------------------- *)
+   Daniel Caille       <daniel.caille@etu.univ-grenoble-alpes.fr>
+   Thomas Crelerot     <thomas.crelerot@etu.univ-grenoble-alpes.fr>
+   Akram Bendouha      <akram.bendouha@etu.univ-grenoble-alpes.fr 
+  --------------------------------------------------------------------------- *)
 
 (* https://github.com/AlexLovser/Project-INF201 *)
 
