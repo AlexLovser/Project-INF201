56012b5d2d89fdd34adaac1e5c6b4701a7dcb998
dfox235 <dcaille390@gmail.com>
2024-03-04 22:26:59 +0100

    ajout text

--- projet_04_03_2024/inf201_ElKortbi_Tabolskii_Bendouha_Caille_Crelerot_Q1-Q9.ml
+++ projet_04_03_2024/inf201_ElKortbi_Tabolskii_Bendouha_Caille_Crelerot_Q1-Q9.ml
@@ -15,6 +15,14 @@
 
 (* https://github.com/AlexLovser/Project-INF201 *)
 
+(*   1.Ce sont les cases du camp sud/ camp de départ du plateau qui satisfont la condition i<-dim;;
+     2.Ce sont les cases du camp nord/ camp d'arrivé du plateau qui satisfont la condition i>dim;; 
+     3 .Ce sont les cases du camp du côté supérieur gauche du plateau qui satisfont la condition j<-dim;;
+     4.C'est la case supérieur du plateau qui satisfait la condition (i,j,k) = (2dim,-dim,-dim);;
+     5.C'est la case supérieur gauche du plan sud/ camp de départ du plateau qui satisfait la condition (i,j,k) = (-dim-1,1,dim);;
+     6.Ce sont toutes les cases du plateau sauf celles des camps sud,supérieur gauche et supérieur droit qui satisfont la conditioni≥-dim ∧ j ≥-dim ∧ k ≥-dim
+
+*)
 
 (* ======================================================================== *)
 (* ======================= DEFINITION DE TYPES ============================ *)
